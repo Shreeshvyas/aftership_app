4da4c7ce93b25cd58de55a1b9ae635f2728de7fc
shreeshvyas <shreeshvyas2@gmail.com>
2024-05-03 12:16:03 +0530

    fixed order type

--- db/migrate/20240502203547_add_column_to_user.rb
+++ db/migrate/20240502203547_add_column_to_user.rb
@@ -2,5 +2,6 @@ class AddColumnToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :slug, :string
     add_column :users, :tag, :string
+    change_column :users, :tracking_number, :integer
   end
 end
